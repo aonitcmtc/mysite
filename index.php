e432d66357653d880d041f4db42a2f92621a9faf
Ubuntu <ubuntu@ip-172-31-1-218.ap-southeast-2.compute.internal>
2025-10-27 18:41:00 +0000

    add info

--- index.php
+++ index.php
@@ -146,7 +146,7 @@
         <div class="d-navbar">
             <nav class="navbar navbar-expand-lg bg-primary-subtle">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="./landing.php">
+                    <a class="navbar-brand" href="../">
                         <img src="./images/weblogo.png" alt="Avatar Logo" style="width:40px;" class="rounded-pill"> 
                     </a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
